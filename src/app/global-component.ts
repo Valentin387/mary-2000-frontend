c80a6f0417a004bdb2f59aa7df6041b17bdc9d1b
Valentin Valencia Valencia <valentinv3v7@gmail.com>
2025-03-30 20:48:34 -0500

    changed navbar title name

--- src/app/global-component.ts
+++ src/app/global-component.ts
@@ -1,4 +1,4 @@
 export const GlobalComponent = {
     // Api Calling
-    API_URL : 'https://mary-2000-backend-bbad22375b6c.herokuapp.com/',
+    API_URL : 'https://mary-2000-backend-bbad22375b6c.herokuapp.com',
 }
